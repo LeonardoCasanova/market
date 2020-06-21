e5797ae92e2af5ebf9fc60106c475d0a4acd3688
LeonardoCasanova <leonardocsanova@gmail.com>
2020-06-21 11:34:46 -0300

    Novas implementacoes e melhorias

--- index.php
+++ index.php
@@ -15,146 +15,96 @@
   <!------ Include the above in your HEAD tag ---------->
 
   <link rel="stylesheet" href="src/css/index.css">
+
 </head>
 <?php
-
 $json_data = json_decode(file_get_contents('src/path/mercados.json'));
 $mercados = $json_data->mercados;
-
 ?>
 
 <body>
-<script type="text/javascript">
-        function readTextFile(file, callback) {
-            var rawFile = new XMLHttpRequest();
-            rawFile.overrideMimeType("application/json");
-            rawFile.open("GET", file, true);
-            rawFile.onreadystatechange = function() {
-                if (rawFile.readyState === 4 && rawFile.status == "200") {
-                    callback(rawFile.responseText);
-                }
-            }
-            rawFile.send(null);
-        }
-
-        function abreModal(id) {
-            readTextFile("src/path/mercados.json", function(text){
-              var data = JSON.parse(text);
-              var fotos = data['mercados'][id-1]['path'];
-              var html =  '';
-
-              var i = 0;
-              for (var k in fotos) {
-                if(i == 0){
-                  html += '<div class="carousel-item active">';
-                  i++;
-                }else{
-                  html += '<div class="carousel-item">';
-                }
-
-                html += '  <img class="d-block w-100" src="'+fotos[k]+'"  alt="Second slide">'+
-                        '</div>';
-              }
-
-
-
-              document.getElementById("pictures").innerHTML = html;
-
-            });
-        }
-
-    </script>
+  <script src="src/js/market.js"> </script>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">Encontre aqui - Promoção de Supermercados</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <!-- <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul> -->
+    </div>
+  </nav>
 
   <div class="bs-example">
-    <div class="container">
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+    <div class="row">
+      <!--- Espaço para Propaganda --->
+      <div class="col-2">
+        <h1>Propagand</h1>
+      </div>
+      <!---Fim Espaço para Propaganda --->
       <br>
       <!-- Mercados -->
-      <section id="team" class="pb-5">
-        <div class="container">
-          <h5 class="section-title h1">Ofertas de Supermercados - Indaiatuba</h5>
-          <div class="row">
-            <?php
-            foreach ($mercados as $mercado) {
-            ?>
-
-              <!-- Card Mercado -->
-              <div class="grid-container col-xs-12 col-sm-6 col-md-4">
-                <div class="mainflip">
-                  <div class="frontside">
-                    <div class="card">
-                      <div class="card-body text-center">
-                        <p><img class=" img-fluid" src="<?php echo $mercado->logo_mercado ?>" alt="card image"></p>
+      <div class="col">
+        <section id="team" class="pb-5">
+          <div class="container">
+            <h5 class="section-title h1">Ofertas de Supermercados - Indaiatuba</h5>
+            <div class="row">
+              <?php
+              foreach ($mercados as $mercado) {
+              ?>
+                <!-- Card Mercado -->
+                <div class="grid-container col-xs-12 col-sm-6 col-md-4">
+                  <div class="mainflip">
+                    <div class="frontside">
+                      <div class="card">
                         <div class="card-body text-center">
-                          <button type="button" class="btn btn-primary" onClick="abreModal(<?php echo $mercado->id; ?>)" data-target="#exampleModal" data-toggle="modal">
-                            Veja as Ofertas
-                          </button>
-                        </div>
-                        <div class="card-body text-center">
-                          <p class="card-text"><?php echo $mercado->data_ofertas ?> </p>
+                          <p><img class=" img-fluid" src="<?php echo $mercado->logo_mercado ?>" height="50px" width="50px" alt="card image"></p>
+                          <div class="card-body text-center">
+                            <button type="button" class="btn btn-primary" onClick="abreModal(<?php echo $mercado->id; ?>)" data-target="#exampleModal" data-toggle="modal">
+                              Veja as Ofertas
+                            </button>
+                          </div>
+                          <div class="card-body text-center">
+                            <p class="card-text"><?php echo $mercado->data_ofertas ?> </p>
 
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <!-- ./Card Mercado-->
+                <!-- ./Card Mercado-->
 
-
-              <!-- Modal -->
-              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-                      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                           <ol class="carousel-indicators">
                             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                           </ol>
-                          <div class="carousel-inner" id="pictures">
-
+                          <div class="zoom">
+                            <div class="carousel-inner" id="pictures">
+                            </div>
                           </div>
                           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -165,32 +115,37 @@ $mercados = $json_data->mercados;
                             <span class="sr-only">Next</span>
                           </a>
                         </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-            <?php
-            }
-            ?>
+              <?php
+              }
+              ?>
 
-
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
       <!-- Mercados -->
-
-      <!-- Footer -->
-        <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
-          <div class="container text-center">
-            <small>Copyright &copy; Your Website</small>
-          </div>
-        </footer>
-      <!-- Footer -->
-
+      <!--- Espaço para Propaganda --->
+      <div class="col-2">
+        <h1>Propagand</h1>
+      </div>
+      <!--- Fim Espaço para Propaganda --->
+    </div>
+  </div>
+  <!-- Footer -->
+  <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; Encontre aqui - 2020</small>
+    </div>
+  </footer>
+  <!-- Footer -->
 </body>
 
 </html>
